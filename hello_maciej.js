d96ecc47358ae33353999af59b5fb23575724152
WojciechStrach <wojciech.strach1@gmail.com>
2016-07-04 12:09:11 +0200

    bla
    bla

--- hello_maciej.js
+++ hello_maciej.js
@@ -1,9 +1,10 @@
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    </head>
+var http = require("http");
+http.createServer(function (request, response) {
 
-    <body>
-        <font color='red'>Hello World!</font>
-    </body>
-</html>
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.end('Hello World\n');
+
+}).listen(8081);
+
+
+console.log('Server running at http://127.0.0.1:8081/');
