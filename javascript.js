db439e8fac81aae040a42872ad8945b6f17ae646
Kamil <kamil.kot.95@gmail.com>
2016-07-05 16:31:24 +0200

    avatars v2

---
+++ javascript.js
@@ -0,0 +1,90 @@
+var response;
+var lastId;
+
+$(document).ready(function () {
+    loadData(false);
+//            addData("x", "y");
+});
+
+function loadData(clear) {
+    $.get("http://192.168.15.105:3000/lol", function (r) {
+        response = r;
+        for (var i = 0; i < response.length; i++) {
+            lastId = response[i].id;
+            // if(clear) {
+            //     for(var i=0; i<1; i++){
+            //         $("#" + i).remove();
+            //     }
+            //
+            // }
+            $("#tbodyy")
+                .append("" +
+                    "<tr id='" + lastId + "'><td><img class=\"avatars\" src=" + response[i].avatarLink + "></td>" +
+                    "<td>" + response[i].name + "</td>" + "<td>" + response[i].surname + "</td></tr>");
+        }
+        $(document).ready(function() {
+            var table = $('#example').dataTable({
+                // ajax: "data.json",
+//                        select: true,
+                "columnDefs": [
+                    {"width": "5%", "targets": 0}
+                ]
+            });
+            $('#example tbody').on( 'click', 'tr', function () {
+                if ( $(this).hasClass('selected') ) {
+                    $(this).removeClass('selected');
+                }
+                else {
+                    table.$('tr.selected').removeClass('selected');
+                    $(this).addClass('selected');
+                }
+            } );
+
+            $('#deleteButton').click( function () {
+                removeData(table.$('tr.selected').attr('id'));
+                table.$('tr.selected')
+                // table.row('.selected').remove().draw( true);
+                // var table = $('#example').dataTable();
+                // $('#example').DataTable().ajax.reload();
+                //
+
+                // table.reload(true, null);
+            } );
+        } );
+    })
+};
+function addData() {
+    $.ajax({
+        type: 'POST',
+        url: 'http://192.168.15.105:3000/lol',
+        data:
+        '{"name":"' +
+        $('#nameField').val() +
+        '", "surname":"' +
+        $('#surnameField').val() +
+        '", "avatar":"' +
+        $('#avatarField').val() +
+        '"}',
+        // success: function(data) { alert('data: ' + data); },
+        contentType: "application/json",
+        dataType: 'json'
+    }).done(function() {
+        // $("#tbodyy")
+        //     .append("" +
+        //         "<tr id='" + lastId + 1 + "'><td><img class=\"avatars\" src=" + response[i].avatarLink + "></td>" +
+        //         "<td>" + response[i].name + "</td>" + "<td>" + response[i].surname + "</td></tr>");
+        loadData(true);
+    });
+}
+function removeData(id) {
+    $.ajax({
+        type: 'DELETE',
+        url: 'http://192.168.15.105:3000/lol/' + id,
+        // success: function(data) { alert('data: ' + data); },
+        // error: function (data) {alert('deleteFail');},
+        contentType: "application/json",
+        dataType: 'json'
+    }).done(function() {
+        $('#' + id).remove();
+    });
+}
