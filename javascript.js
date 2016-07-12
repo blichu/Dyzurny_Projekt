33485ad66b5d582dd87d93609cf502fed7c8c23a
Kamil <kamil.kot.95@gmail.com>
2016-07-12 16:22:38 +0200

    sdf

--- javascript.js
+++ javascript.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
     // });
 
     $( "#calendarContainer").on( "drop", function( event, ui ) {
-        alert(ui.draggable.attr("id"));
+        // alert(ui.draggable.attr("id"));
     } );
     $("#calendarContainer").droppable({
         drop: function(event, ui) {
@@ -56,7 +56,7 @@ $(document).ready(function () {
         var name = $(ui.draggable).children("#name").html();
         var surname = $(ui.draggable).children("#surname").html();
         var avatar = $(ui.draggable).children("#avatar").children("img").attr("src");
-        editData(id, name, surname, avatar);
+        editUser(id, name, surname, avatar);
     } );
     $("#editDragContainer").droppable({
         drop: function(event, ui) {
@@ -191,85 +191,4 @@ function removeData(id) {
     });
 }
 
-function editData(id, tableName, tableSurname, tableAvatar){
-    alert(id);
-    alert(tableName);
-    alert(tableSurname);
-    alert(tableAvatar);
-    w2popup.open({
-        title: 'Popup Title',
-        body: '<div style="height: 80%" class="parentOfDynamicSizeElements">' +
-              '<div id="addUserAvatarView" class="rightDynamicSizeElement"> <!--id="imageView"-->' +
-              '<img id="avatarViewImage" src="../images/noImage.png">' +
-              '</div>' +
-              '<div id="addUserForm" class="leftDynamicSizeElement">' +
-              '<div style="height: 30%;">' +
-              '<label for="nameField" class="addUserFormLabel">Name</label>' +
-              '<input type="text" id="nameField" class="addUserFormInput"/>' +
-              '</div>' +
-              '<div style="height: 5%"></div>' +
-              '<div style="height: 30%;">' +
-              '<label for="surnameField" class="addUserFormLabel">Surname</label>' +
 
-              '<div  class="addUserFormInput" style="width: 30%; float: left">' +
-              '<input type="text" id="surnameField"/>' +
-              '<button id="viewButton" type="button">View</button>' +
-              '</div>' +
-
-              '</div>' +
-              '<div style="height: 5%"></div>' +
-              '<div style="height: 30%;">' +
-              '<label for="avatarURL" class="addUserFormLabel">Avatar URL</label>' +
-              '<input type="text" class="addUserFormInput" id="avatarURL"/>' +
-              '</div>' +
-              '</div>' +
-              '</div>' +
-              '<div style="height: 2%"></div>' +
-              '<div id="addUserButtons">' +
-              '<div id="addButtonDiv" style="float: left">' +
-              '<button id="addButton">Edit</button>' +
-              '</div>' +
-              '<div id="resetButtonDiv" style="float: left">' +
-              '<button id="resetButton">Reset</button>' +
-              '</div>' +
-              '</div>'
-    });
-    viewButtonAction();
-
-    $('#addButton').on("click", function(){
-        var name = $('#nameField').val();
-        var surname = $('#surnameField').val();
-        var avatar = $('#avatarURL').val();
-
-       /*if(name !== "") {
-            alert(name);
-            $.ajax({
-                type: 'UPDATE',
-                url: 'http://localhost:3000/lol/' + id,
-                contentType: "application/json",
-                dataType: 'json',
-                data: JSON.stringify({name: name})
-            })
-        }
-        if(surname !== ""){
-            alert(surname);
-            $.ajax({
-                type: 'PUT',
-                url: 'http://localhost:3000/lol/' + id,
-                contentType: "application/json",
-                dataType: 'json',
-                data: JSON.stringify({name: tableName, surname: surname, avatarLink: tableAvatar})
-            })
-        }
-        if(avatar !== ""){
-            alert(avatar);
-            $.ajax({
-                type: 'PUT',
-                url: 'http://localhost:3000/lol/' + id,
-                contentType: "application/json",
-                dataType: 'json',
-                data: JSON.stringify({name: tableName, surname: tableSurname, avatarLink: avatar})
-            })
-        }*/
-    })
-}
--- javascript/addUserPopup.js
+++ javascript/addUserPopup.js
@@ -16,68 +16,89 @@ function actionRemoveButton() {
     // });
 }
 function actionAddButton() {
-    $('#addButton').click(function () {
+    $('#addButton').on("click", function(){
         var name = $('#nameField').val();
         var surname = $('#surnameField').val();
-        var avatarURL = $('#avatarURL').val();
-        addUserToBase(name, surname, avatarURL)
-        // .done(function( data ) {
-        // addUserToTable(data.id, $('#avatarURL').val(), $('#nameField').val(), $('#surnameField').val());
-        // selectRowAction(tableGlobal);
-        // actionRemoveButton(tableGlobal);
-        addUserToTable(data.id, avatarURL, name, surname, "noAssigned");
-        clearForm();
-        // });
-    });
+        var avatar = $('#avatarURL').val();
+
+        /*if(name !== "") {
+         alert(name);
+         $.ajax({
+         type: 'UPDATE',
+         url: 'http://localhost:3000/lol/' + id,
+         contentType: "application/json",
+         dataType: 'json',
+         data: JSON.stringify({name: name})
+         })
+         }
+         if(surname !== ""){
+         alert(surname);
+         $.ajax({
+         type: 'PUT',
+         url: 'http://localhost:3000/lol/' + id,
+         contentType: "application/json",
+         dataType: 'json',
+         data: JSON.stringify({name: tableName, surname: surname, avatarLink: tableAvatar})
+         })
+         }
+         if(avatar !== ""){
+         alert(avatar);
+         $.ajax({
+         type: 'PUT',
+         url: 'http://localhost:3000/lol/' + id,
+         contentType: "application/json",
+         dataType: 'json',
+         data: JSON.stringify({name: tableName, surname: tableSurname, avatarLink: avatar})
+         })
+         }*/
+    })
 }
 function actionViewButton() {
     $("#viewButton").click(function () {
         document.getElementById("imageView").innerHTML = "<img height='70px' class='avatars' src='" + $('#avatarURL').val() + "'>";
     });
 }
-function editUser(id){
+function editUser(id, tableName, tableSurname, tableAvatar){
+    // alert(id);
+    // alert(tableName);
+    // alert(tableSurname);
+    // alert(tableAvatar);
     w2popup.open({
         title: 'Popup Title',
-        body: '<div style="height: 80%" class="parentOfDynamicSizeElements">' +
-        '<div id="addUserAvatarView" class="rightDynamicSizeElement"> <!--id="imageView"-->' +
-        '<img id="avatarViewImage" src="../images/noImage.png">' +
-        '</div>' +
-        '<div id="addUserForm" class="leftDynamicSizeElement">' +
-        '<div style="height: 30%;">' +
-        '<label for="nameField" class="addUserFormLabel">Name</label>' +
-        '<input type="text" id="nameField" class="addUserFormInput"/>' +
-        '</div>' +
-        '<div style="height: 5%"></div>' +
-        '<div style="height: 30%;">' +
-        '<label for="surnameField" class="addUserFormLabel">Surname</label>' +
-
-        '<div  class="addUserFormInput" style="width: 30%; float: left">' +
-        '<input type="text" id="surnameField"/>' +
-        '<button id="viewButton" type="button">View</button>' +
-        '</div>' +
-
-        '</div>' +
-        '<div style="height: 5%"></div>' +
-        '<div style="height: 30%;">' +
-        '<label for="avatarURL" class="addUserFormLabel">Avatar URL</label>' +
-        '<input type="text" class="addUserFormInput" id="avatarURL"/>' +
-        '</div>' +
-        '</div>' +
+        body:
+        '<div style="height: 80%" class="parentOfDynamicSizeElements">' +
+            '<div id="addUserAvatarView" class="rightDynamicSizeElement"> <!--id="imageView"-->' +
+                '<img id="avatarViewImage" src="../images/noImage.png">' +
+            '</div>' +
+            '<div id="addUserForm" class="leftDynamicSizeElement">' +
+                '<div style="height: 30%;">' +
+                    '<label for="nameField" class="addUserFormLabel">Name</label>' +
+                    '<input type="text" id="nameField" class="addUserFormInput"/>' +
+                '</div>' +
+                '<div style="height: 5%"></div>' +
+                '<div style="height: 30%;">' +
+                    '<label for="surnameField" class="addUserFormLabel">Surname</label>' +
+                    '<input type="text" id="surnameField" class="addUserFormInput"/>' +
+                '</div>' +
+                '<div style="height: 5%"></div>' +
+                '<div style="height: 30%;">' +
+                    '<label for="surnameField" class="addUserFormLabel">Surname</label>' +
+                    '<div class="addUserFormInput">' +
+                        '<input type="text" class="addUserFormInputMain" id="avatarURL"/>' +
+                        '<button id="avatarURLView" class="addUserFormInputShow" type="button">View</button>' +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
         '</div>' +
         '<div style="height: 2%"></div>' +
         '<div id="addUserButtons">' +
-        '<div id="addButtonDiv" style="float: left">' +
-        '<button id="addButton">Add person</button>' +
-        '</div>' +
-        '<div id="resetButtonDiv" style="float: left">' +
-        '<button id="resetButton">Reset</button>' +
-        '</div>' +
+            '<div id="addButtonDiv" style="float: left">' +
+                '<button id="addButton">Edit</button>' +
+            '</div>' +
+            '<div id="resetButtonDiv" style="float: left">' +
+                '<button id="resetButton">Reset</button>' +
+            '</div>' +
         '</div>'
     });
     actionViewButton();
-
-    $('#addButton').on("click", function(){
-        var name = $('#nameField').val();
-        editUser(name);
-    })
 }
